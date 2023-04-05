02033bad35525d5c810a87ef66dfe4d4c9c79e05
Salvador Cabral <122478917+salvador96c@users.noreply.github.com>
2023-04-05 17:07:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+let userName = 'Salvador'
+
+userName != '' ? console.log(`Hello ${userName}!`) : console.log('Hello!')
+
+const userQuestion = 'Will it rain?'
+
+console.log(`${userQuestion}`)
+
+const randomNumber = Math.floor(Math.random() * 8)
+
+// console.log(randomNumber)
+
+let eightBall = ''
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+  default:
+    eightBall = '';
+}
+
+console.log(eightBall)
